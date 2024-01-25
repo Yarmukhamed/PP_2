c78cf940a8169708d3e1a45a1dc5597a91b5071c
Yarmukhamed <y_yarmukhamed@kbtu.kz>
2024-01-25 11:26:20 +0600

    add some files

--- Lab1/variables/example variables.py
+++ Lab1/variables/example variables.py
@@ -101,8 +101,13 @@ print("Python is " + x)
 def myfunc():
   global x
   x = "fantastic"
-
 myfunc()
+print("Python is " + x)
 
+x = "awesome"
+def myfunc():
+  global x
+  x = "fantastic"
+myfunc()
 print("Python is " + x)
 
