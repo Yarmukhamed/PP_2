3d8783219b093639f2f58838d5ab02a0d54cdc5a
Yarmukhamed <y_yarmukhamed@kbtu.kz>
2024-02-14 22:24:53 +0600

    add

---
+++ lab4/json/example.py
@@ -0,0 +1,122 @@
+#1
+import json
+x =  '{ "name":"John", "age":30, "city":"New York"}'
+y = json.loads(x)
+
+print(y["age"])
+#2
+import json
+x = {
+  "name": "John",
+  "age": 30,
+  "city": "New York"
+}
+
+y = json.dumps(x)
+print(y)
+
+#3
+import json
+
+print(json.dumps({"name": "John", "age": 30}))
+print(json.dumps(["apple", "bananas"]))
+print(json.dumps(("apple", "bananas")))
+print(json.dumps("hello"))
+print(json.dumps(42))
+print(json.dumps(31.76))
+print(json.dumps(True))
+print(json.dumps(False))
+print(json.dumps(None))
+#4
+import json
+
+x = {
+  "name": "John",
+  "age": 30,
+  "married": True,
+  "divorced": False,
+  "children": ("Ann","Billy"),
+  "pets": None,
+  "cars": [
+    {"model": "BMW 230", "mpg": 27.5},
+    {"model": "Ford Edge", "mpg": 24.1}
+  ]
+}
+
+y = json.dumps(x)
+print(y)
+
+#5
+import json
+x = {
+  "name": "John",
+  "age": 30,
+  "married": True,
+  "divorced": False,
+  "children": ("Ann","Billy"),
+  "pets": None,
+  "cars": [
+    {"model": "BMW 230", "mpg": 27.5},
+    {"model": "Ford Edge", "mpg": 24.1}
+  ]
+}
+
+print(json.dumps(x, indent=4, separators=(". ", " = ")))
+​
+{
+    "name" = "John".
+    "age" = 30.
+    "married" = true.
+    "divorced" = false.
+    "children" = [
+        "Ann".
+        "Billy"
+    ].
+    "pets" = null.
+    "cars" = [
+        {
+            "model" = "BMW 230".
+            "mpg" = 27.5
+        }.
+        {
+            "model" = "Ford Edge".
+            "mpg" = 24.1
+        }
+    ]
+}
+#6
+import json
+
+x = {
+  "name": "John",
+  "age": 30,
+  "married": True,
+  "divorced": False,
+  "children": ("Ann","Billy"),
+  "pets": None,
+  "cars": [
+    {"model": "BMW 230", "mpg": 27.5},
+    {"model": "Ford Edge", "mpg": 24.1}
+  ]
+}
+
+print(json.dumps(x, indent=4, separators=(". ", " = ")))
+
+#7
+import json
+
+x = {
+  "name": "John",
+  "age": 30,
+  "married": True,
+  "divorced": False,
+  "children": ("Ann","Billy"),
+  "pets": None,
+  "cars": [
+    {"model": "BMW 230", "mpg": 27.5},
+    {"model": "Ford Edge", "mpg": 24.1}
+  ]
+}
+
+print(json.dumps(x, indent=4, sort_keys=True))
+
